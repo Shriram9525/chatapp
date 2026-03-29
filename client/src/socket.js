b0c6b882eab4b764ea1c74c1df0c25bd30a45d80
Shriram9525 <shrirambyrappa2005@gmail.com>
2026-03-29 15:38:38 +0530

    fix socket for deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
-// Use the environment variable for the API URL, or fallback to localhost for development
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const socket = io(API_URL, {
-    autoConnect: false, // Connect manually when needed
+    autoConnect: false,
+    transports: ["websocket"], // 🔥 important for Render
 });
 
 export default socket;
